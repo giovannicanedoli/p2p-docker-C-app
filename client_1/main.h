b0420c6756429b4ed2f5293dc7beb4158e232b6f
giovannicanedoli <gio.canedoli02@gmail.com>
2024-05-12 20:24:53 +0200

    aggiunto .h

---
+++ client_1/main.h
@@ -0,0 +1,9 @@
+#define NAMESIZE 100
+#define MESSAGESIZE 1000
+#define NPEERS 10
+
+void handle_error(char* msg);
+void *receive_fun(void* server_fd);
+void recvdata(int socket);
+void send_data_to_server(const char* msg);
+void send_data_to_peer(const char* msg, const char* peer);
